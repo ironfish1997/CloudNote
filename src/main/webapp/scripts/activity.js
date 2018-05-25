edf56e85766f0f05050d520b333e85d60bdec872
刘立勇 <1481980097@qq.com>
2018-05-25 14:56:27 +0800

    web

---
+++ src/main/webapp/scripts/activity.js
@@ -0,0 +1,63 @@
+﻿/***
+ * 获得活动列表
+ */
+function getActivityList(){
+	alert("获得活动列表");
+}
+
+/***
+ * 查询指定活动下已参加活动的笔记列表
+ */
+function getNoteActivitys(){
+	alert("查询参加活动的笔记列表");
+}
+
+/***
+ * 查询活动笔记内容
+ */
+function getNoteActivityDetail(){
+	console.log("查询活动笔记内容");
+}
+
+/***
+ * 查询可选择的笔记本
+ */
+function getSelectNoteBook(){
+	alert("查询可选择的笔记本");
+}
+
+/***
+ * 查询可选择的笔记
+ */
+function getSelectNoteList(){
+	alert("查询可选择的笔记");
+}
+
+/***
+ *	将用户选择的笔记参加活动
+ */
+function createNoteActivity(){
+	alert("将用户选择的笔记参加活动");
+	$('.close,.cancle').trigger('click');
+}
+
+/***
+ *	分享活动笔记
+ */
+function likeActivityNote() {
+	alert("分享活动笔记");
+}
+
+/***
+ *	顶笔记
+ */
+function up() {
+	alert("顶笔记");
+}
+
+/***
+ *	踩笔记
+ */
+function down(noteActivityId, dom) {
+	alert("踩笔记");
+}
---
+++ src/main/webapp/scripts/notebook.js
@@ -0,0 +1,41 @@
+/***
+ * 加载普通笔记本
+ */
+function loadNormalNoteBook(){
+	alert("加载普通笔记本");
+}
+
+/***
+ * 加载特殊笔记本
+ */
+function loadSpecialNoteBook(){
+	alert("加载特殊笔记本");
+}
+
+/****
+ * 添加笔记本
+ */
+function addNoteBook(){
+	alert("添加笔记本");
+}
+
+/***
+ * 重命名笔记本
+ */
+function updateNoteBook(){
+	alert("重命名笔记本");
+}
+
+/***
+ * 删除笔记本
+ */
+function deleteNoteBook(){
+	alert("删除笔记本");
+}
+
+/**
+ * 将笔记本列表放置到select组件中
+ */
+function setNoteBookToSelect(){
+	console.log("将笔记本列表放置到select组件中");
+}
