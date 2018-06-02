266a8f6df2795d508770b427f18cbded3270d2ca
刘立勇 <1481980097@qq.com>
2018-06-02 22:38:29 +0800

    实现从数据库读出所有笔记的方法，实现删除笔记的方法，实现把笔记放入垃圾桶的方法

--- src/main/java/controller/NoteController.java
+++ src/main/java/controller/NoteController.java
@@ -24,6 +24,13 @@ public class NoteController extends AbstractController {
         return new JsonResult(list);
     }
 
+    @RequestMapping("/listAll.do")
+    @ResponseBody
+    public Object listAll(){
+        List<Map<String, Object>> list = noteService.listAllNotes();
+        return new JsonResult(list);
+    }
+
     @RequestMapping("/addNote.do")
     @ResponseBody
     public Object addNote(String notebookId, String userId, String title, String body) {
@@ -32,13 +39,31 @@ public class NoteController extends AbstractController {
 
     @RequestMapping("/updateNote.do")
     @ResponseBody
-    public JsonResult updateNote(String noteId,  String title,String body ) {
+    public JsonResult updateNote(String noteId,  String title,String body) {
         return new JsonResult(noteService.updateNote(noteId, title, body));
+    }
+
+    @RequestMapping("/moveNote.do")
+    @ResponseBody
+    public JsonResult moveNote(String noteId, String notebookId){
+        return new JsonResult(noteService.updateNote(noteId,notebookId));
     }
 
     @RequestMapping("/getNoteContent.do")
     @ResponseBody
     public JsonResult getNoteContentByNoteId(String noteId){
         return new JsonResult(noteService.getNoteContent(noteId));
+    }
+
+    @RequestMapping("/trashNote.do")
+    @ResponseBody
+    public JsonResult trashNote(String noteId,String statusId){
+        return new JsonResult(noteService.trashNote(noteId,statusId));
+    }
+
+    @RequestMapping("/deleteNote.do")
+    @ResponseBody
+    public JsonResult deleteNote(String noteId){
+        return new JsonResult(noteService.deleteNote(noteId));
     }
 }
